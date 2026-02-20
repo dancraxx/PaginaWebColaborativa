13d51d9ab1478ef27463de552f8fdd9e99db7746
Alejandro Torres González <alextggithub@gmail.com>
2026-02-20 12:40:37 +0100

    Código JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,34 @@
+let display = document.getElementById("visual");
+
+function appendNumber(number) {
+    if (display.value === "0") {
+        display.value = number;
+    } else {
+        display.value += number;
+    }
+}
+
+function appendOperator(operator) {
+    let lastChar = display.value.slice(-1);
+    if ("+-*/".includes(lastChar)) {
+        display.value = display.value.slice(0, -1) + operator;
+    } else {
+        display.value += operator;
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
